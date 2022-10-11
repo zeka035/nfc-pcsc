dc2a4f4bfb9072e51e48f0e757450cb3fd21e855
Miloš <zeka035@gmail.com>
2022-10-11 21:01:59 +0200

    fix

--- src/NFC.js
+++ src/NFC.js
@@ -1,6 +1,6 @@
 "use strict";
 
-import pcsclite from '@pokusew/pcsclite';
+import pcsclite from '@zeka035/pcsclite';
 import EventEmitter from 'events';
 import Reader from './Reader';
 import ACR122Reader from './ACR122Reader';
